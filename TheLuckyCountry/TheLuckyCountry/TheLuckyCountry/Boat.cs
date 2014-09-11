89d25d9ebe690924fbc61b2efebeb50a47df1277
Shane Brouwer <shane@findesign.com.au>
2014-09-11 12:44:40 +1000

    x

---
+++ TheLuckyCountry/TheLuckyCountry/TheLuckyCountry/Boat.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.flixel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace TheLuckyCountry
+{
+    class Boat : FlxSprite
+    {
+
+        public Boat(int xPos, int yPos)
+            : base(xPos, yPos)
+        {
+            loadGraphic(FlxG.Content.Load<Texture2D>("boats"), true, false, 16, 16);
+
+            addAnimation("up", new int[] { 0,1 }, (int)FlxU.random(4,8), true);
+            addAnimation("right", new int[] { 18, 19 }, (int)FlxU.random(4, 8), true);
+            addAnimation("down", new int[] { 36, 37 }, (int)FlxU.random(4, 8), true);
+            addAnimation("left", new int[] { 54, 55 }, (int)FlxU.random(4, 8), true);
+            addAnimation("stopped", new int[] { 36 }, (int)FlxU.random(4, 8), true);
+
+
+            play("right");
+        }
+
+        override public void update()
+        {
+            if (velocity.X > 0)
+            {
+                double rot = Math.Atan2((float)velocity.Y, (float)velocity.X);
+                double degrees = rot * 180 / Math.PI;
+
+                angle = (float)degrees;
+            }
+            else
+            {
+                double rot = Math.Atan2((float)velocity.Y, (float)velocity.X);
+                double degrees = rot * 180 / Math.PI;
+
+                angle = (float)degrees;
+            }
+
+            if (angle > 0 && angle < 90)
+            {
+                play("right");
+            }
+            else if (angle > 90 && angle < 180)
+            {
+                play("down");
+            }
+            else if (angle > -180 && angle < -90)
+            {
+                play("left");
+            }
+            else if (angle > -90 && angle < 0)
+            {
+                play("up");
+            }
+
+
+            angle = 0;
+
+            if (velocity.X == 0)
+            {
+                play("stopped");
+            }
+
+
+            base.update();
+
+        }
+
+
+    }
+}
---
+++ TheLuckyCountry/TheLuckyCountry/TheLuckyCountry/IntroState.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using org.flixel;
+
+using System.Linq;
+using System.Xml.Linq;
+
+namespace TheLuckyCountry
+{
+    public class IntroState : FlxState
+    {
+        private FlxGroup tileGrp;
+        private FlxGroup boats;
+
+        override public void create()
+        {
+            FlxG.resetHud();
+            FlxG.hideHud();
+
+            FlxG.backColor = Color.DarkTurquoise;
+
+            base.create();
+
+
+
+            FlxCaveGeneratorExt caveExt = new FlxCaveGeneratorExt(100, 60, 0.42f, 3);
+            string[,] caveLevel = caveExt.generateCaveLevel();
+
+            tileGrp = new FlxGroup();
+
+            for (int i = 0; i < caveLevel.GetLength(1); i++)
+            {
+                for (int y = 0; y < caveLevel.GetLength(0); y++)
+                {
+                    //string toPrint = tiles[y, i];
+                    if (Convert.ToInt32(caveLevel[y, i]) != 0)
+                    {
+                        FlxSprite x = new FlxSprite(i * 8, y * 8);
+                        //x.createGraphic(8, 8, colors[Convert.ToInt32(caveLevel[y, i])]);
+                        x.loadGraphic("flixel/autotilesIsland", false, false, 8, 8);
+                        //x.color = colors[Convert.ToInt32(caveLevel[y, i])];
+
+                        x.frame = Convert.ToInt32(caveLevel[y, i]);
+                        //x.scale = 2;
+                        x.angularDrag = 250;
+                        //x.setOffset(4, 4);
+
+                        x.@fixed = true;
+                        tileGrp.add(x);
+                    }
+                    //Console.Write(toPrint);
+                }
+
+                //Console.WriteLine();
+            }
+
+            //string newMap = caveExt.convertMultiArrayStringToString(caveLevel);
+
+
+            add(tileGrp);
+
+            boats = new FlxGroup();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Boat b = new Boat((int)FlxU.random(0, FlxG.width), (int)FlxU.random(0, FlxG.height));
+                b.velocity.X = FlxU.random(-40, 40);
+                b.velocity.Y = FlxU.random(-40, 40);
+
+
+                boats.add(b);
+            }
+
+            add(boats);
+        }
+
+        override public void update()
+        {
+
+
+            FlxU.collide(tileGrp, boats);
+
+            base.update();
+        }
+
+
+    }
+}
